e80726e35b714533a518f2657e63971fbed480dc
hDn24 <hodangnhan24@gmail.com>
2023-03-03 16:16:05 +0700

    Added visualize function

--- utils.py
+++ utils.py
@@ -26,3 +26,68 @@ KEYPOINT_EDGE_INDS_TO_COLOR = {
     (12, 14): (255, 255, 0),
     (14, 16): (255, 255, 0),
 }
+
+
+def visualize(
+    image: np.ndarray,
+    list_persons: List[Person],
+    keypoint_color: Tuple[int, ...] = (0, 255, 0),
+    keypoint_threshold: float = 0.05,
+    instance_threshold: float = 0.1,
+) -> np.ndarray:
+    """Draws landmarks and edges on the input image and return it.
+    Args:
+        image: The input RGB image.
+        list_persons: The list of all "Person" entities to be visualize.
+        keypoint_color: the colors in which the landmarks should be plotted.
+        keypoint_threshold: minimum confidence score for a keypoint to be drawn.
+        instance_threshold: minimum confidence score for a person to be drawn.
+    Returns:
+        Image with keypoints and edges.
+    """
+    for person in list_persons:
+        if person.score < instance_threshold:
+            continue
+
+        keypoints = person.keypoints
+        bounding_box = person.bounding_box
+
+        # Draw all the landmarks
+        for i in range(len(keypoints)):
+            if keypoints[i].score >= keypoint_threshold:
+                cv2.circle(image, keypoints[i].coordinate, 2, keypoint_color, 4)
+
+        # Draw all the edges
+        for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
+            if (
+                keypoints[edge_pair[0]].score > keypoint_threshold
+                and keypoints[edge_pair[1]].score > keypoint_threshold
+            ):
+                cv2.line(
+                    image,
+                    keypoints[edge_pair[0]].coordinate,
+                    keypoints[edge_pair[1]].coordinate,
+                    color,
+                    2,
+                )
+
+        # Draw bounding_box with multipose
+        if bounding_box is not None:
+            start_point = bounding_box.start_point
+            end_point = bounding_box.end_point
+            cv2.rectangle(image, start_point, end_point, keypoint_color, 1)
+            # Draw id text when tracker enabled for MoveNet MultiPose model.(
+            # id = -1 with single pose model or when tracker is None)
+            if person.id != -1:
+                id_text = "id = " + str(person.id)
+                cv2.putText(
+                    image,
+                    id_text,
+                    start_point,
+                    cv2.FONT_HERSHEY_PLAIN,
+                    1,
+                    (0, 0, 255),
+                    1,
+                )
+
+    return image
