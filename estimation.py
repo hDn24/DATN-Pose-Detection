e6169f557de462657cc127709df2a256028240cf
hDn24 <hodangnhan24@gmail.com>
2023-03-03 14:50:35 +0700

    Set width and height for camera

--- estimation.py
+++ estimation.py
@@ -1,19 +1,21 @@
 import cv2
 
 
-def main():
+def main(width: int = 640, height: int = 480) -> None:
     camera_id = 0
     # Start capturing video input from the camera
     cap = cv2.VideoCapture(camera_id)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     while cap.isOpened():
-        _, img = cap.read()
+        _, image = cap.read()
 
         # Stop the program if the ESC key is pressed.
         if cv2.waitKey(1) == 27:
             break
 
-        cv2.imshow("img", img)
+        cv2.imshow("image", image)
 
     cap.release()
     cv2.destroyAllWindows()
